b2fd1278544e6f2220a0cddeac841ba12da6984b
Alexandr Xoxon <4218820@gmail.com>
2023-02-04 12:27:38 +0300

    changed color

--- Navigation/Extension/UIColorExtension.swift
+++ Navigation/Extension/UIColorExtension.swift
@@ -13,7 +13,7 @@ extension UIColor {
         #colorLiteral(red: 0.3236978054, green: 0.1063579395, blue: 0.574860394, alpha: 1)
     }
     static var mainHeavenly: UIColor {
-        #colorLiteral(red: 0.3098039216, green: 0.6274509804, blue: 0.9803921569, alpha: 1)
+        #colorLiteral(red: 0.7458103554, green: 0.8569280509, blue: 0.9803921569, alpha: 1)
     }
     static var tabBarItemLight: UIColor {
         #colorLiteral(red: 0.3236978054, green: 0.1063579395, blue: 0.574860394, alpha: 0.5084592301)
